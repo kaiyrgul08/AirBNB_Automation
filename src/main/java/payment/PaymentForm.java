62d1cbf5e46551cb5b7b2856cb6e12fffd3d769b
kai <baialievakaiyrgul@gmail.com>
2024-12-25 11:21:23 +0600

    feature/Payment form

---
+++ src/main/java/payment/PaymentForm.java
@@ -0,0 +1,13 @@
+package payment;
+
+public class PaymentForm {
+        public static void main(String[] args) {
+            int sum = addNumbers(5, 10);
+            System.out.println("Сумма: " + sum); // Вывод результата
+        }
+
+        public static int addNumbers(int a, int b) {
+            return a + b; // Возврат суммы
+        }
+    }
+
